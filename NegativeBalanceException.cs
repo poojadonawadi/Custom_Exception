6b347524b55a44cd922167a59cf7d46df243dbd0
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-27 20:23:28 +0530

    [Pooja] added: Custom exception

---
+++ NegativeBalanceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Exception
+{
+    internal class NegativeBalanceException:Exception
+    {
+        public NegativeBalanceException(string message):base(message) { }
+
+        public override string ToString()
+        {
+            return $"{Message}";
+        }
+    }
+}
