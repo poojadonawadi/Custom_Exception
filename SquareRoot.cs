10d6ee5202937c6a0b74fb78dfa79b2ee2b615ff
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-27 20:30:12 +0530

    [Pooja] added: square root

---
+++ SquareRoot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Exception
+{
+    internal class SquareRoot
+    {
+        public static void GetSqareRoot(int num)
+        {
+            try
+            {
+                if(num <= 0)
+                {
+                    throw new ZeroNotAllowedException("Zero or less than zero is not allowed");
+                }
+                double result = Math.Sqrt(num);
+                Console.WriteLine($"The square root of {num} is {result}");
+            }
+            catch(ZeroNotAllowedException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }               
+        }
+    }
+}
